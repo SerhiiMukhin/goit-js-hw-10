bce82f53d76f81fef11737ba7c8e768f5e8dc2d2
SerhiiMukhin <sergio.lerone.ua@gmail.com>
2023-01-04 17:30:11 +0200

    draft of hw-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix, { Notify } from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+// fetchCountries('ukraine')
+// .then(country => console.log(country))
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput() {
+  let inputValue = input.value.trim();
+  // console.log(inputValue)
+  fetchCountries(inputValue)
+    .then(countries => renderMarkup(countries))
+    .catch(error => console.log(error));
+}
+
+function renderMarkup(countries) {
+  if (countries.length >= 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+  if (countries.length >= 2 && countries.length < 10) {
+    Notify.info('From 2 to 10 countries left');
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = countries
+      .map(
+        ({ flags, name }) =>
+          `
+      <li class="item"><img
+      src="${flags.svg}"
+      alt="${name.official}"
+      width="60"
+      height="40"
+      class="country-flag">
+      <p class="item-info">${name.official}</p>
+      </li>
+      `
+      )
+      .join('');
+    //ТУТ БУДЕ ВИВОДИТИСЯ СПИСОК КРАЇН
+  }
+  if (countries.length === 1) {
+    Notify.info('You find your country!');
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = countries
+      .map(
+        ({ flags, name, capital, population, languages }) =>
+          `
+        <img src="${flags.svg}" alt="${
+            name.official
+          }" width="60" height="40" class="country-flag">
+        <h1 class="country-name">${name.official}</h1>
+        <p class="info-item">
+        <span class="accent">Capital: </span>${capital}
+        </p>
+        <p class="info-item">
+        <span class="accent">Population: </span>${population} humans
+        </p>
+        <p class="info-item">
+        <span class="accent">Languages: </span>${Object.values(languages).join(
+          ', '
+        )}
+        </p>
+      `
+      )
+      .join('');
+    // ТУТ БУДЕ ІНФОРМАЦІЯ ПРО КРАЇНУ
+  }
+}
